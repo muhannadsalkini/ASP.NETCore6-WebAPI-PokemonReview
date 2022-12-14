4d93ba33c702035e08c2ef2186bc8c9647d06a6a
muhannadsalkini <mohanad.salkini@gmail.com>
2022-12-14 23:59:46 +0300

    Added Entity Framework

---
+++ P1_PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_PokemonReviewApp.Models;
+
+namespace P1_PokemonReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        // 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        // Database Tables
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Pokemon-Category Many-To-Many Relationship
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.CategoryId);
+
+            // Pokemon-Owner Many-To-Many Relationship
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(po => po.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(po => po.PokemonOwners)
+                .HasForeignKey(o => o.OwnerId);
+        }
+    }
+}
+
+// Type ctor and double tab to creat a constructer
--- P1_PokemonReviewApp/Models/PokemonOwner.cs
+++ P1_PokemonReviewApp/Models/PokemonOwner.cs
@@ -3,8 +3,8 @@
     public class PokemonOwner
     {
         // Class for Pokemon and Owner Many-To-Many relationship join table
-        public int PokemonId { get; set; } // Id number
-        public int OwnerId { get; set; }
+        public int PokemonId { get; set; } // PokemonId <-> Owner
+        public int OwnerId { get; set; } // OwnerId <-> Pokemon
         public Pokemon Pokemon { get; set; } // Actual enttity
         public Owner Owner { get; set; }
     }
