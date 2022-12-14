b6906b9f7732b8b4ff73a325ddd3a01ecb70f795
muhannadsalkini <mohanad.salkini@gmail.com>
2022-12-14 14:05:38 +0300

    Added Models

---
+++ P1_PokemonReviewApp/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace P1_PokemonReviewApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ P1_PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace P1_PokemonReviewApp.Models
+{
+    public class Pokemon // table
+    {
+        public int Id { get; set; } // column
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
+
+// Write prop than double-tab Tab to creat a Property faster. You can switch using tab also.
