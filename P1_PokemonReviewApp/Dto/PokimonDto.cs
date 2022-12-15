398187243410569ac90c880a08cbddfd6ccfca0e
muhannadsalkini <mohanad.salkini@gmail.com>
2022-12-15 21:44:46 +0300

    Added DTO

---
+++ P1_PokemonReviewApp/Dto/PokimonDto.cs
@@ -0,0 +1,9 @@
+﻿namespace P1_PokemonReviewApp.Dto
+{
+    public class PokimonDto
+    {
+        public int Id { get; set; } // column
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
